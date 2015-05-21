4ab72c268de26bebf447d6eea65bda857455d9bb
zhanshanqin <zhanshanqinw@126.com>
2015-05-22 02:29:28 +0800

    20150521

--- fruit-web/js/app.js
+++ fruit-web/js/app.js
@@ -10,5 +10,21 @@
     $(document).on($.AMUI.fullscreen.raw.fullscreenchange, function() {
       $.AMUI.fullscreen.isFullscreen ? $fullText.text('关闭全屏') : $fullText.text('开启全屏');
     });
+
+
+
   });
 })(jQuery);
+
+(function (doc, win) {
+    var docEl = doc.documentElement,
+    resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+    recalc = function () {
+      var clientWidth = docEl.clientWidth;
+      if (!clientWidth) return;
+      docEl.style.fontSize = 50 * (clientWidth / 320) + 'px';//假设360宽度的时候，1rem=20px，
+    };
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
